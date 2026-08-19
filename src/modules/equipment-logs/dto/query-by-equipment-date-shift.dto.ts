f4f781ed7212dc96abd79806d3543c5956c635d1
andri-hermawan <andri.hermawan.skom@gmail.com>
2026-08-19 10:11:06 +0700

    feat:update two enpoint in equipment-logs

--- src/modules/equipment-logs/dto/query-by-equipment-date-shift.dto.ts
+++ src/modules/equipment-logs/dto/query-by-equipment-date-shift.dto.ts
@@ -1,13 +1,18 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsOptional, IsString } from 'class-validator';
 
-export class QueryByDateShiftDto {
+export class QueryByEquipmentDateShiftDto {
   @ApiPropertyOptional({
     description: 'Filter by created_at date (YYYY-MM-DD)',
   })
   @IsOptional()
   @IsString()
   created_at?: string;
+
+  @ApiPropertyOptional({ description: 'Filter by equipment_code' })
+  @IsOptional()
+  @IsString()
+  equipment_code?: string;
 
   @ApiPropertyOptional({ description: 'Filter by shift name' })
   @IsOptional()
